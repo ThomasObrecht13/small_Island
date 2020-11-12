d786d092df0f27cdf1591f90cf9bcd69ab8d4c0c
Thomas Obrecht <thomas.obrecht@edu.univ-fcomte.fr>
2020-11-12 18:51:33 +0100

    Lecture CarteObjectif

--- test/CarteObjectifUnitTest.java
+++ test/CarteObjectifUnitTest.java
@@ -10,7 +10,15 @@ public class CarteObjectifUnitTest {
 
     @Test
     public void lectureCarteRecomenseTest(){
-        CarteRecompense recompense = new CarteRecompense(2,333,12,22,10,4);
-        Assert.assertEquals("2 temple / 333 port / 12 lotus / 22 dragon / 10 arka / 4 emplacement vide ",recompense.lectureRecompense());
+        CarteRecompense recompense = new CarteRecompense(2,333,12,22,10,4,2);
+        Assert.assertEquals("2 temple / 333 port / 12 lotus / 22 dragon / 10 arka / 4 emplacement vide / 2 bonus ",recompense.lectureRecompense());
+    }
+
+    @Test
+    public void lectureCarteObjectifTest(){
+        CarteMission mission = new CarteMission(1,503,40,17,2);
+        CarteRecompense recompense = new CarteRecompense(2,333,12,22,10,4,2);
+        CarteObjectif objectif = new CarteObjectif(mission,recompense);
+        Assert.assertEquals("Objetif : 1 temple / 503 port / 40 lotus / 17 dragon / 2 arka // Récompense : 2 temple / 333 port / 12 lotus / 22 dragon / 10 arka / 4 emplacement vide / 2 bonus ",objectif.lectureCarteObjectif());
     }
 }
