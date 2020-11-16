3cfbf6ed55ec2e253382c22cca67117e3e9d4996
Hugo Weber <hugo.weber@edu.univ-fcomte.fr>
2020-11-16 16:49:33 +0100

    Commit supprimer classe carte

--- src/Partie.java
+++ src/Partie.java
@@ -53,7 +53,7 @@ public class Partie {
                     CarteObjectif c11 = new CarteObjectif();
                     CarteObjectif c12 = new CarteObjectif();
                     //
-                    List<CarteMission> co = Arrays.asList(c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12);
+                    List<CarteObjectif> co = Arrays.asList(c1,c2,c3,c4,c5,c6,c7,c8,c9,c10,c11,c12);
                     //
                     cartesObj.addAll(co);
                     break;
